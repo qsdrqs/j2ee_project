b19d84f001e308cae2024c473825e512f60c05a1
qsdrqs <qsdrqs@gmail.com>
2020-07-03 12:37:11 +0800

    初始化项目

---
+++ src/main/java/grp/wudi/j2ee/J2eeApplication.java
@@ -0,0 +1,13 @@
+package grp.wudi.j2ee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2eeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grp/wudi/j2ee/J2eeApplicationTests.java
@@ -0,0 +1,13 @@
+package grp.wudi.j2ee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
