bd707fca3fdd214bdb8f7d9293b7bf2be345d187
Einsam <1079949564@qq.com>
2020-07-06 17:42:00 +0800

    newsdetil还有缺陷

--- src/main/webapp/front_pages/front_vue/src/components/news_details.vue
+++ src/main/webapp/front_pages/front_vue/src/components/news_details.vue
@@ -1,13 +1,71 @@
 <template>
   
+    <div class="news_details" >
+        <el-container>
+            <el-header>{{newinfo.title}}</el-header>
+            <el-main v-html="newsinfo.content"></el-main>
+        </el-container>
+    </div>
 </template>
 
 <script>
 export default {
+    name:"news_details",
+    data(){
+        return{
+            id:this.$route.params.id,
+            newinfo:{}
+        };
+    },
+    created(){
+        this.getNewsDetails();
+    },
+    methods:{
+        getNewsDetails(){
+            this.$http.get('api/getnew'+this.id).then(result => {
+                if(result.body.status === 0){
+                    this.newinfo=result.body.message[0];
+                }else{
+                    alert("shibai");
+                }
+            })
+            
+        }
+    }
 
 }
 </script>
 
 <style>
-
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    margin-left: 15%;
+    margin-right: 15%;
+  }
+  
+  
+  .el-main {
+    background-color: #E9EEF3;
+    padding: 6px 4px;
+    color: #333;
+    text-align: center;
+    margin-left: 15.5%;
+    margin-right: 15.5%;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
